7a2bae5322780353af4d524b90b1b46b1c11e20c
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-14 23:50:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,10 @@
 class Book
-  attr_accessor :title, :genre
+  attr_accessor :title, :author, :page_count, :genre
   
-  def initialize (title, genre)
+  def initialize (title, author, page_count, genre)
     @title = title
+    @author = author
+    @page_count = page_count
     @genre = genre
-  end
+ end
 end
