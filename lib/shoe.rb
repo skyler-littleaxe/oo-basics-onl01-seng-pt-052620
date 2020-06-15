c5bd18d0757b72c2c3a4e79ce891fc18a9dbbe99
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-15 00:03:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize (brand)
+    @brand = brand
+  end  
+  
+  def cobble
+    @condition = new
+    puts "The she has been repaired"
+end  
